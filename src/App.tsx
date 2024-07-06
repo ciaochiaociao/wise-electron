190695134018d502c6972dd521cb6f2f0c7e021d
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-07-05 19:16:42 -0700

    add chatbot UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import { MyChatbot } from './components/Chatbot/Chatbot'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://electron-vite.github.io" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -30,7 +31,8 @@ function App() {
       </p>
       <h1 className="text-3xl font-bold underline">
       Hello world!
-      </h1>
+      </h1> */}
+      <MyChatbot />
     </>
   )
 }
---
+++ src/components/Chatbot/Chatbot.tsx
@@ -0,0 +1,18 @@
+import config from './config.js';
+import MessageParser from './MessageParser.jsx';
+import ActionProvider from './ActionProvider.jsx';
+
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+
+export const MyChatbot = () => {
+  return (
+    <div>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+};
