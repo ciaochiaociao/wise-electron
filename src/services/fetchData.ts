f78ce7654f9e251d6187769caa5cf7eb7d6e46cb
ciaochiaociao <13917478+ciaochiaociao@users.noreply.github.com>
2024-08-16 10:27:17 -0700

    add services that fetch data from backend

---
+++ src/services/fetchData.ts
@@ -0,0 +1,19 @@
+interface EmotionRecord {
+  emotion: string
+  confidence: number
+  created_at: string
+}
+
+async function fetchEmotionRecords(): Promise<EmotionRecord[] | undefined> { 
+// fetch emotion data from the server with error handling
+  try {
+    const response = await fetch("http://localhost:8000/emotionrecords/?format=json")
+    if (!response.ok) throw new Error('Failed to fetch emotion data from the server')
+    const data: EmotionRecord[] = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default fetchEmotionRecords
